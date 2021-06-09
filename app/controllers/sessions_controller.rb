42cd6ca649337d33c9b8a205a99b8c0f24ba0cae
Ben Hayes <b.t.hayes@comcast.net>
2021-06-09 18:03:45 -0500

    added errors to incorrect login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    redirect_to @user
+    if User.find_by(username: params[:username])
+      @user = User.find_by(username: params[:username])
+      @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash[:notice] = "Incorrect username or password"
+      redirect_to '/login'
+    end
   end
 
   def destroy
